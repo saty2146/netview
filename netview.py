a28749d0be7134680ed4f8931c2f3f5bd679ade1
saty2146 <tibor.klimo@gmail.com>
2017-04-06 14:52:54 +0200

    small changes

--- netview.py
+++ netview.py
@@ -6,7 +6,7 @@ from flask import request
 from datetime import datetime
 import pandas as pd
 import numpy as np
-import re, glob, os, subprocess
+import re, glob, os, subprocess, time
 
 ip_whitelist = ['81.89.63.129', '81.89.63.131', '127.0.0.1']
 
@@ -27,6 +27,11 @@ def get_data():
 
     return out
 
+def last_update():
+    newest = max(glob.glob('./scripts/files/*.csv'), key=os.path.getmtime)
+    result = str(time.ctime(os.path.getmtime(newest)))
+    return result
+
 @app.route("/run")
 def run():
     return render_template('run.html', subprocess_output=get_data())
@@ -39,33 +44,26 @@ def show_mac():
     filename = [os.path.basename(i) for i in (files)]
     ids = [i for i in range(len(filename))]
     form.file_option.choices = list(zip(ids, filename))
+
     if form.validate_on_submit():
         find_item = form.find_item.data.strip()
         file_id = form.file_option.data
-        print find_item, file_id
-        print type(file_id)
         selected_file = [f[1] for f in form.file_option.choices if f[0] == file_id]
-        print selected_file
         data = pd.read_csv('./scripts/files/' + selected_file[0], dtype={'vlan': str})
     else:
         find_item = None
-        print "NOT VALIDATED"
         newest = max(glob.glob('./scripts/files/mac*.csv'), key=os.path.getmtime)
     
         data = pd.read_csv(newest, dtype={'vlan': str})
+
     machost = request.args.get('host', default = 'six1')
-    #test = request.form.get('file_option', default='nic')
     hosts = set(list(data['host']))
-    #form.file_option.choices = list(zip(ids, filename))
-    #file_option = form.file_option.choices
-    print form.file_option.choices
+
     if find_item:
+
         columns = ['host', 'vlan', 'type', 'mac', 'iface']
-#        mask = np.column_stack([data[col].str.lower().str.contains(post, na=False) for col in data])
         mask = np.column_stack([data[col].str.contains(find_item, flags=re.IGNORECASE, na=False) for col in columns])
         data = data.loc[mask.any(axis=1)]
-
-        #data = data.loc[data['ip'] == post]
     else:
         data = data[data.host == machost]
     return render_template('macs.html',table=data.to_html(index=False, classes='table table-hover'), title = 'MACs', form = form, hosts = hosts, find_item = find_item )
@@ -82,14 +80,11 @@ def show_vlans():
     newest = max(glob.glob('./scripts/files/vlan*.csv'), key=os.path.getmtime)
     data = pd.read_csv(newest, dtype={'vlan': str})
     data.info()
-    #data.set_index('vlan', inplace=True)
-    #males = data.loc[data.Gender=='m']
+
     if post:
         columns = ['vlan', 'name', 'six1', 'six2', 'sit1', 'sit2', 'shc31', 'shc32', 'n31', 'n32', 'n41', 'n42']
         mask = np.column_stack([data[col].str.contains(post, flags=re.IGNORECASE, na=False) for col in columns])
         data = data.loc[mask.any(axis=1)]
-
-        #data = data.loc[data['ip'] == post]
     else:
         pass
     return render_template('vlans.html',table=data.to_html(index=False, classes='table table-hover'), title = 'Vlans', form=form, post = post)
@@ -116,32 +111,28 @@ def show_arp():
         data = pd.read_csv(newest)
 
     arphost = request.args.get('host', default = 'six1')
-    print arphost
-    #test = request.form.get('file_option', default='nic')
     hosts = set(list(data['host']))
-    #form.file_option.choices = list(zip(ids, filename))
-    #file_option = form.file_option.choices
-    print form.file_option.choices
+
     if find_item:
+
         columns = ['host', 'ip', 'mac', 'iface']
-#        mask = np.column_stack([data[col].str.lower().str.contains(post, na=False) for col in data])
         mask = np.column_stack([data[col].str.contains(find_item, flags=re.IGNORECASE, na=False) for col in columns])
         data = data.loc[mask.any(axis=1)]
 
-        #data = data.loc[data['ip'] == post]
     else:
         data = data[data.host == arphost]
+
     return render_template('arps.html',table=data.to_html(index=False, classes='table table-hover'), title = 'ARPs', form = form, hosts = hosts, find_item = find_item )
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
-def show_home():
+def show_index():
+    update = last_update()
 
     if valid_ip():
-        return render_template('index.html', title = 'Home')
+        return render_template('index.html', title = 'Home', update = update)
     else:
         return render_template('404.html', title = 'Not Found')
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='81.89.63.131')
